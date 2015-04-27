69283ebeeca3d949afd05a224af97ca2a6fa0a6f
Zhantong Zhang <zhantongz@gmail.com>
2015-04-27 01:55:41 +0000

    get url from date and time

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
 import re
+import subprocess
+import urllib.request
+import datetime
+from scipy.constants.constants import year
 
-search_engine = "http://www.assembly.ab.ca/documents/isysmenu.html"
+def search_url(data_dict):
+    engine = 'http://www.assembly.ab.ca/documents/isysmenu.html'
+    
+    data_items = []
+    for n, v in data_dict.items():
+        data_items.append('='.join((n, v)))
+    
+    data = '&'.join(data_items).encode('ascii')
+    r = urllib.request.urlopen(engine, data)
+    
+    return r.geturl()
 
-def get_hansard_html(date) :    
-    IW_DATABASE ='i_hansards';
-    IW_FIELD_INPUT = 'Title';
-    IW_FILTER_FNAME_LIKE = '20150326_1330_01_han.pdf'
-    IW_FIELD_OK = 'Search+in+Hansard'
+def get_hansard_url(date):
+    fdate = date.strftime('%Y%m%d')
+    ftime = date.strftime('%H%M')
+    
+    fname = '_'.join((fdate, ftime, '01_han.pdf'))
+    
+    data_dict = {'IW_DATABASE': 'i_hansards',
+                 'IW_FIELD_INPUT': 'Title',
+                 'IW_FILTER_FNAME_LIKE': fname,
+                 'IW_FIELD_OK': 'Search+in+Hansard'}
+    
+    return search_url(data_dict)
+    
+def process_hansard(file_name):
+    return None
     
 if __name__ == '__main__':
+    year = int(input('year: '))
+    month = int(input('month: '))
+    day = int(input('day: '))
+    time = input('time (morning/afternoon/evening): ')
+    
+    if time == 'morning':
+        time = datetime.time(8, 30)
+    elif time == 'afternoon':
+        time = datetime.time(13, 30)
+    else:
+        time = datetime.time(19, 30)
+    
+    date = datetime.datetime.combine(datetime.date(year, month, day), time)
+    print(get_hansard_url(date))
