748cc342a609654177700603492291994231ca6b
Zhantong Zhang <zhantongz@gmail.com>
2015-04-29 10:20:49 +0000

    licence info

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python3
 
+# Copyright (c) 2015 Zhan Tong Zhang
+# Data scraper for Alberta Hansard
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import re
 import subprocess
 import urllib.request
@@ -40,10 +61,13 @@ def process_hansard(html_obj):
     html = str(html_obj).replace(r'\r\n', '\n')
 
     # remove useless information (styles and mail-in info)
-    content = re.split(r'.*(?=Province of Alberta<br \/>)', html, re.DOTALL)
-    content = re.split(r'If your address is incorrect', content[0])
+    content = re.split('html>[\s\S]*(?=Province of Alberta<br \/>)', html)
+    content = re.split(r'If your address is incorrect',
+                       content[1])[0].replace('<br />', '\n')
+    with open('output.txt', 'w') as file:
+        file.write(content)
 
-    print(start[0])
+    print(content)
 
     return None
 
